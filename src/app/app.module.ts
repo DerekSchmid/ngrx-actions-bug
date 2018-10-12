586120a061ab803aa514ba23597628eb44731505
Derek K Schmid <derek@srsolutions.biz>
2018-10-12 19:02:41 -0400

    Updated Karma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { NgrxActionsModule } from 'ngrx-actions';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './components/left.component';
+import { RightComponent } from './components/right.component';
+import { effects, reducers, reset } from './store';
+
+let devtools = [];
+if (!environment.production) {
+  devtools = [StoreDevtoolsModule.instrument({ maxAge: 25 })];
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RightComponent, LeftComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgrxActionsModule,
+    StoreModule.forRoot(reducers, { metaReducers: [reset] }),
+    EffectsModule.forRoot([...effects]),
+    ...devtools,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [...effects],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
